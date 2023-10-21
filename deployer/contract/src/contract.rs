b34369c595819361d5955a0eaa4821fcf2f66fc0
Markeljan Sokoli <Markeljan19@gmail.com>
2023-10-21 11:44:11 -0400

    latest logo

--- deployer/contract/src/contract.rs
+++ deployer/contract/src/contract.rs
@@ -3,41 +3,32 @@
 multiversx_sc::imports!();
 
 #[multiversx_sc::contract]
-pub trait Contract {
-    #[view(getSum)]
-    #[storage_mapper("sum")]
-    fn sum(&self) -> SingleValueMapper<BigUint>;
+pub trait CounterContract {
+    #[view(getCounter)]
+    #[storage_mapper("counter")]
+    fn counter(&self) -> SingleValueMapper<BigUint>;
 
     #[init]
     fn init(&self) {
-        self.sum().set(&BigUint::zero());
+        self.counter().set(&BigUint::zero());
     }
 
     #[endpoint]
-    fn add(&self, value: BigUint) {
-        self.sum().update(|sum| *sum += value);
+    fn increment(&self) {
+        self.counter().update(|counter| *counter += 1u32);
     }
 
     #[endpoint]
-    fn subtract(&self, value: BigUint) {
-        self.sum().update(|sum| {
-            if *sum > value {
-                *sum -= value;
-            } else {
-                *sum = BigUint::zero();
+    fn decrement(&self) {
+        self.counter().update(|counter| {
+            if *counter > BigUint::zero() {
+                *counter -= 1u32;
             }
         });
     }
 
     #[endpoint]
-    fn multiply_by_2(&self) {
-        self.sum().update(|sum| *sum *= 2u32);
-    }
-
-    #[endpoint]
-    fn add_random(&self) {
-        let mut rand_source = RandomnessSource::new();
-        let random_value = BigUint::from(rand_source.next_u16() as u32);
-        self.sum().update(|sum| *sum += random_value);
+    fn reset(&self) {
+        self.counter().set(&BigUint::zero());
     }
 }
